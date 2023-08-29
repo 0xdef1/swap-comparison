2d91031d0785accaf28fb773898e58faecd1523b
0xdef1 <0xdef1.eth@gmail.com>
2023-08-29 09:58:06 -0400

    Fix cowswap

--- src/fetch-quotes.ts
+++ src/fetch-quotes.ts
@@ -3,6 +3,25 @@ const UNIV3_QUOTER_ABI = require('../abi/univ3-quoter-abi.json')
 const TRICRYPTO_ABI = require('../abi/tricrypto-abi.json')
 const TRICRYPTO_ADDRESS = '0xD51a44d3FaE010294C616388b506AcdA1bfAAE46' // ETH Mainnet Default
 const QUOTER_ADDRESS = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6' // ETH Mainnet Default
+import _ from 'lodash'
+
+export async function uniswapQuoteAllTiers(
+    block: number, 
+    sellAssetAddress: string, 
+    buyAssetAddress: string,
+    amount: string,
+    web3: Web3,
+    quoterAddress = QUOTER_ADDRESS,
+    ): Promise<string> {
+
+    let quotes = await Promise.all([500,3000,10000].map(tier => {
+        return uniswapQuote(block, sellAssetAddress, buyAssetAddress, tier, amount, web3, quoterAddress)
+    }));
+
+    let output = _(quotes).filter(v => v != '').orderBy(v => parseInt(v), 'desc').valueOf()[0];
+    return output
+
+}
 
 export async function uniswapQuote(
     block: number, 
@@ -24,9 +43,10 @@ export async function uniswapQuote(
             amount,
             0
         ).call({}, block)
+        console.log('Successful uniswap quote:', block, sellAssetAddress, buyAssetAddress, feeTier, amount)
     } catch (e) {
         console.log('Failed uniswap quote: ', block, sellAssetAddress, buyAssetAddress, feeTier, amount)
-        throw e
+        //throw e
     }
     return output
 }
